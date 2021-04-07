3ceaf389abe84ef2b373b2649846a300fd698f8c
Yang Yu <81887269+yancy001@users.noreply.github.com>
2021-04-07 17:37:03 -0600

    Update SHAP results as figures.py

--- AutoEncoder and SHAP/SHAP results as figures.py
+++ AutoEncoder and SHAP/SHAP results as figures.py
@@ -1,3 +1,5 @@
+# path - is a string to desired path location. The file does
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,9 +8,12 @@ from sklearn import preprocessing
 from sklearn.ensemble import RandomForestRegressor
 import shap
 
+PATH_TO_DATA = './data.txt'    #path to data
+PATH_TO_AE_RESULT = './AE_199.txt'    #path to AutoEncoder results, alwarys the last epoch result
+PATH_TO_SAVE_FIGURE = '.figure.pdf'     #path to save SHAP figure
 RNA_name = 'BRCA'
-gene = pd.read_csv('/export/home/yang.yu2/UK_Biobank_data/TumorOnly_QC/'+RNA_name+'_TumorOnly_QC.csv',index_col=0)
-hidden_vars = pd.read_csv('/export/home/yang.yu2/UK_Biobank_code/pytorch_projects/Linear_AE_Geno/'+RNA_name+'199.csv',header = None)
+gene = pd.read_csv(PATH_TO_DATA ,index_col=0)
+hidden_vars = pd.read_csv(PATH_TO_AE_RESULT,header = None)
 
 X_train, X_test, Y_train, Y_test = train_test_split(gene,
                                                 hidden_vars[0],
@@ -21,4 +26,4 @@ my_model.fit(X_train, Y_train)
 shap_values = shap.TreeExplainer(my_model).shap_values(X_test)
 
 shap.summary_plot(shap_values, X_test, plot_type="bar")
-plt.savefig('/export/home/yang.yu2/UK_Biobank_code/figure/shap_BRCA.pdf', format='pdf')
+plt.savefig(PATH_TO_SAVE_FIGURE, format='pdf')
