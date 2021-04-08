e1dc38e73f39588ffc26b2193f32deea675fb5fd
Yang Yu <81887269+yancy001@users.noreply.github.com>
2021-04-07 18:28:15 -0600

    Update threshold for SHAP results.py

--- AutoEncoder and SHAP/threshold for SHAP results.py
+++ AutoEncoder and SHAP/threshold for SHAP results.py
@@ -1,8 +1,13 @@
+# path - is a string to desired path location. The file does
+
 import numpy as np
 import pandas as pd
 
+PATH_TO_SHAP_RESULT = './shap.txt'    #path to SHAP results
+PATH_TO_SAVE = './shap_new.txt'     #path to save SHAP results after threshold
+
 threshold = 0.05
-gene = pd.read_csv('/export/home/yang.yu2/UK_Biobank_code/figure/BRCA_20000.txt',sep = '\t',header = None)
+gene = pd.read_csv(PATH_TO_SHAP_RESULT,sep = '\t',header = None)
 geneid = pd.DataFrame(columns = [0,1])
 
 for i in range(len(gene)):
@@ -10,4 +15,4 @@ for i in range(len(gene)):
     geneid = geneid.append(gene.iloc[i],ignore_index = True)
 
 geneid = geneid[0]
-geneid.to_csv('/export/home/yang.yu2/UK_Biobank_code/figure/ORA/BRCA_20000_'+str(threshold)+'.txt',header = 0, index = 0, sep = '\t')
+geneid.to_csv(PATH_TO_SAVE,header = 0, index = 0, sep = '\t')
