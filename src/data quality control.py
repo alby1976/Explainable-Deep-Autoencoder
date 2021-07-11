ee14aaebae8458c25782e430afcc7d099763c91c
Yang Yu <81887269+yancy001@users.noreply.github.com>
2021-07-10 22:49:16 -0600

    Update data quality control.py

--- src/data quality control.py
+++ src/data quality control.py
@@ -1,10 +1,11 @@
-# path - is a string to desired path location. 
+## Use Python for data preprocessing (quality control)
+## path - is a string to desired path location. 
 
 import numpy as np
 import pandas as pd
 
-PATH_TO_DATA = './data.txt'    #path to data (before quality control)
-PATH_TO_SAVE = './data_QC.txt'.          #path to save data after quality control
+PATH_TO_DATA = './data.txt'           #path to data (before quality control)
+PATH_TO_SAVE = './data_QC.txt'.       #path to save data after quality control
 
 Tumor = pd.read_csv(PATH_TO_DATA,index_col=0)
 Tumor_var = Tumor.var()
