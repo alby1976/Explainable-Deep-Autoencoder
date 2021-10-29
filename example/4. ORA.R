2b8c8d320b870d7f07e50508dc3e7cfcdbe176c7
Yang Yu <81887269+yancy001@users.noreply.github.com>
2021-10-29 11:53:31 -0600

    Create 4. ORA.R

---
+++ example/4. ORA.R
@@ -0,0 +1,32 @@
+## R code for Over-Representation Analysis
+
+library(WebGestaltR)
+compress_num <- 512 # number of files need to be tested
+
+gene_module <- '' # gene module name without number
+PATH_TO_SAVE <- '' # path to save ORA results
+pathway_num <- 10 # the minimum number of pathways to save
+
+for (i in 1:compress_num){
+  skip_to_next <- FALSE
+  tryCatch({
+    num <- as.character(i)
+    file_name <- paste0(gene_module, i, ".csv")
+    curr_data <- read.csv(file_name, sep="\t", header = FALSE)
+    ORA <- WebGestaltR(interestGene = (curr_data$V1),
+                        enrichMethod = "ORA",
+                        fdrThr = 0.05,
+                        minNum = 5,
+                        maxNum = 1000,
+                        organism = "hsapiens",
+                        enrichDatabase="pathway_KEGG",
+                        interestGeneType="ensembl_gene_id", referenceSet = "genome",
+                        referenceGeneType = "ensembl_gene_id", isOutput = TRUE,
+                        projectName = "DiffExpr")}, error = function(e) {skip_to_next <<- TRUE}
+  )
+  if (skip_to_next) {next}
+  else if (!is.null(ORA) && nrow(ORA) >= pathway_num) {
+    path_to_save <- paste(PATH_TO_SAVE, i,'.csv', sep = '')
+    write.csv(ORA, path_to_save)
+  } else { next}
+}
