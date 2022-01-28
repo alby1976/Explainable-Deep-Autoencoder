a01f0470e00c2b058de6b299bc4cf7acf962bb0e
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-01-28 12:15:30 -0700

    Fixed Logical error in GeneSelectionByPathway.py
    There was a logical error of getting the geneID from
    the gene name. Also changed the function parameters of
    get_gene_ids and get_gene_ids_from_string so that
    the Ensemble Release version can be dynamic.

--- src/GeneSelectionByPathway.py
+++ src/GeneSelectionByPathway.py
@@ -4,29 +4,32 @@ import pandas as pd
 import numpy as np
 
 
-def get_gene_ids_from_string(genes: str) -> np.ndarray:
-    return get_gene_ids(np.fromstring(genes, dtype=str, sep=';'))
+def get_gene_ids_from_string(ensembl_release: int, genes: str) -> np.ndarray:
+    return get_gene_ids(ensembl_release=ensembl_release, gene_list=np.fromstring(genes, dtype=str, sep=';'))
 
 
-def get_gene_ids(gene_list: np.ndarray) -> np.ndarray:
-    gene_data = EnsemblRelease(104)
+def get_gene_ids(ensembl_release: int, gene_list: np.ndarray) -> np.ndarray:
+    gene_data = EnsemblRelease(ensembl_release)
     ids = []
     for gene in gene_list:
         ids.append(gene_data.gene_ids_of_gene_name(gene_name=gene)[0])
     return np.array(ids)
 
 
-# data setup
-PATH_TO_DATA = '../data_example.csv'  # path to original data
-PATH_TO_PATHWAY_DATA = './data_example_QC.csv'  # path to pathway data
-PATH_TO_SAVE_FILTERED_DATA = ' '  # path to saved filtered original data
+if __name__ == '__main__':
+    # data setup
+    path_to_data = '../data_example.csv'  # path to original data
+    path_to_pathway_data = './data_example_QC.csv'  # path to pathway data
+    path_to_save_filtered_data = ' ./'  # path to saved filtered original data
+    ensembl_version = 104  # Ensembl Release version
 
-geno = pd.read_csv(PATH_TO_DATA, index_col=0)  # original data
-pathways = pd.read_csv(PATH_TO_PATHWAY_DATA)
-pathways.All_Genes = pathways.All_Genes.map(get_gene_ids)
-table = {}
-# filter data
-for index, gene_set in enumerate(pathways.All_Genes):
-    table[pathways.iloc[index + 1, 0]] = geno[geno.columns.intersection(gene_set)]
+    geno = pd.read_csv(path_to_data, index_col=0)  # original data
+    pathways = pd.read_csv(path_to_pathway_data)
+    pathways.All_Genes = pathways.All_Genes.apply(lambda x:
+                                                  get_gene_ids_from_string(ensembl_release=ensembl_version, genes=x))
+    table = {}
+    # filter data
+    for index, gene_set in enumerate(pathways.All_Genes):
+        table[pathways.iloc[index + 1, 0]] = geno[geno.columns.intersection(gene_set)]
 
-# process filtered dataset
+    # process filtered dataset
