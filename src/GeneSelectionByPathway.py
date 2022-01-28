260b2c704788d6c19a8ad4494da1b1d1248eb357
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-01-28 09:02:36 -0700

    Updated GeneSelectionByPathway.py
    Implemented the functionality to take the pathway.csv file and filter the
    input datafile that match the genes for the corresponding pathway.

--- src/GeneSelectionByPathway.py
+++ src/GeneSelectionByPathway.py
@@ -1,12 +1,19 @@
 # Filters Dataset by KEGG Pathway and uses PyEnsembl to get EnsemblID
-from numpy import ndarray
 from pyensembl import EnsemblRelease
 import pandas as pd
 import numpy as np
 
 
-def get_gene_list(genes: str) -> np.ndarray:
-    return np.fromstring(genes, dtype=int, sep=' ')
+def get_gene_ids_from_string(genes: str) -> np.ndarray:
+    return get_gene_ids(np.fromstring(genes, dtype=str, sep=';'))
+
+
+def get_gene_ids(gene_list: np.ndarray) -> np.ndarray:
+    gene_data = EnsemblRelease(104)
+    ids = []
+    for gene in gene_list:
+        ids.append(gene_data.gene_ids_of_gene_name(gene_name=gene)[0])
+    return np.array(ids)
 
 
 # data setup
@@ -15,9 +22,11 @@ PATH_TO_PATHWAY_DATA = './data_example_QC.csv'  # path to pathway data
 PATH_TO_SAVE_FILTERED_DATA = ' '  # path to saved filtered original data
 
 geno = pd.read_csv(PATH_TO_DATA, index_col=0)  # original data
-pathway = pd.read_csv(PATH_TO_PATHWAY_DATA)
-gene_list: ndarray = get_gene_list(pathway.All_Genes)
-gene_data = EnsemblRelease(104)
-ids = np.array([])
-for gene in gene_list:
-    np.append(ids, gene_data.gene_ids_of_gene_name(gene_name=gene)[0])
+pathways = pd.read_csv(PATH_TO_PATHWAY_DATA)
+pathways.All_Genes = pathways.All_Genes.map(get_gene_ids)
+table = {}
+# filter data
+for index, gene_set in enumerate(pathways.All_Genes):
+    table[pathways.iloc[index + 1, 0]] = geno[geno.columns.intersection(gene_set)]
+
+# process filtered dataset
