2cbf3f06359eceacf3e8505cdc89628dce882ec3
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-01-28 08:36:29 -0700

    Fixed logical error in AutoEncoderModule.py
    Changed 'x' to 'X' for np.savetxt function and refactored AutoEncoder.py
    to meet Python coding standards.

--- src/AutoEncoder.py
+++ src/AutoEncoder.py
@@ -1,5 +1,6 @@
 ## Use Python to run Deep Autoencoder (feature selection)
 ## path - is a string to desired path location.
+import math
 
 import pandas as pd
 import numpy as np
@@ -7,45 +8,46 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
-import os
-from .AutoEncoderModule import GPDataSet
-from .AutoEncoderModule import AutoGenoShallow
-from .AutoEncoderModule import run_ae
+from src.AutoEncoderModule import GPDataSet
+from src.AutoEncoderModule import AutoGenoShallow
+from src.AutoEncoderModule import run_ae
 
-PATH_TO_DATA = './data.txt'      #path to original data
-PATH_TO_SAVE_AE = './AE/'      #path to save AutoEncoder results
-PATH_TO_SAVE_QC = './data_QC.txt'       #path to save original data after quality control
+def main():
+    path_to_data = '../data_example.csv'  # path to original data
+    path_to_save_ae = '../AE/'  # path to save AutoEncoder results
+    path_to_save_qc = '../data_QC.csv'  # path to save original data after quality control
 
-model_name = 'AE_Geno'
-save_dir = PATH_TO_SAVE_AE
-if not os.path.exists(save_dir):
-    os.mkdir(save_dir)
+    model_name = 'AE_Geno'
 
-geno = pd.read_csv(PATH_TO_DATA,index_col=0) #data quality control
-geno_var = geno.var()
-geno.drop(geno_var[geno_var < 1].index.values, axis=1, inplace=True)
-geno.to_csv(PATH_TO_SAVE_QC)
-geno = np.array(geno)
-snp = int(len(geno[0]))
+    geno = pd.read_csv(path_to_data, index_col=0)  # data quality control
+    geno_var = geno.var()
+    geno.drop(geno_var[geno_var < 1].index.values, axis=1, inplace=True)
+    geno.to_csv(path_to_save_qc)
+    geno = np.array(geno)
+    snp = int(len(geno[0]))
 
-batch_size = 4096
-geno_train, geno_test = train_test_split(geno, test_size=0.1, random_state=42)
+    batch_size = 4096
+    geno_train, geno_test = train_test_split(geno, test_size=0.1, random_state=42)
+
+    geno_train_set = GPDataSet(geno)
+    geno_train_set_loader = DataLoader(dataset=geno_train_set, batch_size=batch_size, shuffle=False, num_workers=8)
+
+    geno_test_set = GPDataSet(geno_test)
+    geno_test_set_loader = DataLoader(dataset=geno_test_set, batch_size=batch_size, shuffle=False, num_workers=8)
+    input_features = int(snp)
+    output_features = input_features
+    smallest_layer = math.ceil(snp / 500)
+    hidden_layer = int(2 * smallest_layer)
+
+    model = AutoGenoShallow(input_features=input_features, hidden_layer=hidden_layer,
+                            smallest_layer=smallest_layer, output_features=output_features).cuda()
+    distance = nn.MSELoss()  # for regression, 0, 0.5, 1
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    run_ae(model=model, save_dir=path_to_save_ae, model_name=model_name, input_features=input_features,
+           smallest_layer=smallest_layer, geno_train_set_loader=geno_train_set_loader,
+           geno_test_set_loader=geno_test_set_loader, distance=distance,
+           optimizer=optimizer, do_train=True, do_test=True)
 
 
-geno_train_set = GPDataSet(geno)
-geno_train_set_loader = DataLoader(dataset=geno_train_set, batch_size=batch_size, shuffle=False, num_workers=8)
-
-geno_test_set = GPDataSet(geno_test)
-geno_test_set_loader = DataLoader(dataset=geno_test_set, batch_size=batch_size, shuffle=False, num_workers=8)
-input_features = int(snp)
-output_features = input_features
-smallest_layer = int(snp / 20000)
-hidden_layer = int(2*smallest_layer)
-
-model = AutoGenoShallow(input_features=input_features, hidden_layer=hidden_layer,
-                        smallest_layer=smallest_layer, output_features=output_features).cuda()
-distance = nn.MSELoss()  # for regression, 0, 0.5, 1
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-run_ae(model=model, model_name=model_name, input_features= input_features, smallest_layer=smallest_layer,
-       geno_train_set_loader=geno_train_set_loader, geno_test_set_loader=geno_test_set_loader, distance=distance,
-       optimizer=optimizer, do_train=True, do_test=True)
+if __name__ == '__main__':
+    main()
--- src/AutoEncoderModule.py
+++ src/AutoEncoderModule.py
@@ -1,6 +1,6 @@
 import time
 from typing import Any
-
+import os
 from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import Dataset
@@ -54,8 +54,14 @@ class AutoGenoShallow(nn.Module):
         return x, y
 
 
+def create_dir (save_dir: str):
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+
+
 def run_ae (model_name, model, geno_train_set_loader, geno_test_set_loader, input_features, smallest_layer, optimizer,
             distance=nn.MSELoss(), num_epochs=200, batch_size=4096, do_train=True, do_test=True, save_dir='./'):
+    create_dir(save_dir)
     for epoch in range(num_epochs):
         start_time = time.time()
         batch_precision_list = []
@@ -90,7 +96,7 @@ def run_ae (model_name, model, geno_train_set_loader, geno_test_set_loader, inpu
             coder_np = np.array(output_coder_list)
             temp = round(smallest_layer / 1000)
             coder_file = f"{save_dir}{model_name}-{str(epoch)}.csv"
-            np.savetxt(fname=coder_file, x=coder_np, fmt='%f', delimiter=',')
+            np.savetxt(fname=coder_file, X=coder_np, fmt='%f', delimiter=',')
             average_precision = sum(
                 batch_precision_list) / current_batch  # precision_list is a list of [ave_pre_batch1, ave_pre_batch2,...]
         # ===========test==========
@@ -116,6 +122,6 @@ def run_ae (model_name, model, geno_train_set_loader, geno_test_set_loader, inpu
                 test_batch_precision_list.append(batch_average_precision)  # [ave_pre_batch1, ave_pre_batch2,...]
             test_average_precision = sum(
                 test_batch_precision_list) / test_current_batch  # precision_list is a list of [ave_pre_batch1, ave_pre_batch2,...]
-        print(f"epoch[{epoch +1}/{num_epochs}, loss: {sum_loss:.4f}, precision: {average_precision:.4f}, "
+        print(f"epoch[{epoch +1:3d}/{num_epochs}, loss: {sum_loss:.4f}, precision: {average_precision:.4f}, "
               f"test precision: {test_average_precision:.4f}")
 
