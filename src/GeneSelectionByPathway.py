b5719f0f268472e12a5325e6c8384dd130d320c9
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-01-30 23:56:41 -0700

    Updated GeneSelectionByPathway.py
    File contains the skeleton to be run from the command line and to create
    slurm jobs.

--- src/GeneSelectionByPathway.py
+++ src/GeneSelectionByPathway.py
@@ -2,6 +2,8 @@
 from pyensembl import EnsemblRelease
 import pandas as pd
 import numpy as np
+import subprocess
+import sys
 
 
 def get_gene_ids_from_string(ensembl_release: int, genes: str) -> np.ndarray:
@@ -18,13 +20,14 @@ def get_gene_ids(ensembl_release: int, gene_list: np.ndarray) -> np.ndarray:
 
 if __name__ == '__main__':
     # data setup
-    path_to_data = '../data_example.csv'  # path to original data
-    path_to_pathway_data = './data_example_QC.csv'  # path to pathway data
-    path_to_save_filtered_data = ' ./'  # path to saved filtered original data
-    ensembl_version = 104  # Ensembl Release version
+    ensembl_version = int(sys.argv[1])  # Ensembl Release version e.g. 104
+    path_to_original_data = sys.argv[2]  # path to original data e.g. './data/input/'
+    pathway_data = sys.argv[3]  # pathway data e.g. './data/pathway.csv'
+    path_to_save_filtered_data = sys.argv[4]  # base directory to saved filtered original data e.g. './data/filter'
+    save_dir = sys.argv[5]  # directory to save AE models
 
     geno = pd.read_csv(path_to_data, index_col=0)  # original data
-    pathways = pd.read_csv(path_to_pathway_data)
+    pathways = pd.read_csv(pathway_data)
     pathways.All_Genes = pathways.All_Genes.apply(lambda x:
                                                   get_gene_ids_from_string(ensembl_release=ensembl_version, genes=x))
     table = {}
@@ -33,3 +36,10 @@ if __name__ == '__main__':
         table[pathways.iloc[index + 1, 0]] = geno[geno.columns.intersection(gene_set)]
 
     # process filtered dataset
+    command = ['python AutoEncoder.py', ensembl_version, filename, path_to_pathway_data, path_to_save_filtered_data, save_dir]
+    output = subprocess.run([sys.executable, '-c', f'sbatch {job_file}'], capture_output=True, text=True, check=True)
+    print('####################')
+    print('Return code:', output.returncode)
+    # use decode function to convert to string
+    print('Output:', output.stdout)
+
