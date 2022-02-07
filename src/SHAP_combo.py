f27ef8906a1c25b0336ebf7748a2189825614d85
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-07 09:09:19 -0700

    Refactored SHAP_combo.py GeneSelectionByPathway.py
    SHAP_combo.py has been automated to accept command line arguments
    GeneSelectionByPathway.py's  sbatch is able to call SHAP_combo.py

--- src/SHAP_combo.py
+++ src/SHAP_combo.py
@@ -1,61 +1,79 @@
-## Use Python to plot SHAP figure (include both bar chart and scatter chart) and generate gene module based on SHAP value
-from typing import Union, Any
+# Use Python to plot SHAP figure (include both bar chart and scatter chart) and generate gene module based on SHAP value
+import sys
+from pathlib import Path
+from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import shap
-from pandas import Series, DataFrame
-from pandas.io.parsers import TextFileReader
+from pandas import DataFrame
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
-# Input
-PATH_TO_DATA_GENE_NAME = './gene_name_QC.csv'  # path to cleaned data with gene annotation (not gene id) (after quatlity control)
-PATH_TO_DATA_GENE_ID = './gene_id_QC.csv'  # path to cleaned data with gene id (not gene name) (after quality control)
-PATH_TO_AE_RESULT = './AE_199.csv'  # path to AutoEncoder results, always the last epoch result
 
-# Output
-PATH_TO_SAVE_BAR: str = './shap/bar'  # path to save SHAP bar chart
-PATH_TO_SAVE_SCATTER: str = './shap/scatter'  # path to save SHAP scatter chart
-PATH_TO_SAVE_GENE_MODULE: str = './shap/gene_module'  # path to save gene module
+def main(path_to_data_gene_name: Path, path_to_data_gene_id: Path, path_to_ae_result: Path,
+         path_to_save_bar: Path, path_to_save_scatter: Path, path_to_save_gene_model: Path):
+    gene: DataFrame = pd.read_csv(path_to_data_gene_name, index_col=0)
+    hidden_vars: DataFrame = pd.read_csv(path_to_ae_result, header=None)
+    column_num: int = len(hidden_vars.columns)
+    sample_num: int = len(gene.index)
+    top_rate: float = 1 / 20  # top rate of gene columns
+    top_num: int = int(top_rate * len(gene.columns))
+    gene_id: DataFrame = pd.read_csv(path_to_data_gene_id, index_col=0, header=True)
+    gene_id: np.ndarray = np.array(gene_id.columns)
 
-gene: Union[Union[TextFileReader, Series, DataFrame, None], Any] = pd.read_csv(PATH_TO_DATA_GENE_NAME, index_col=0)
-hidden_vars: Union[Union[TextFileReader, Series, DataFrame, None], Any] = pd.read_csv(PATH_TO_AE_RESULT, header=None)
-column_num: int = len(hidden_vars.columns)
-sample_num: int = len(gene.index)
-top_rate: float = 1 / 20  # top rate of gene columns
-top_num: int = int(top_rate * len(gene.columns))
-gene_id: DataFrame = pd.read_csv(PATH_TO_DATA_GENE_ID, index_col=0, header=True)
-gene_id = np.array(gene_id.columns)
+    for i in range(column_num):
+        X_train: Any
+        X_test: Any
+        Y_train: Any
+        Y_test: Any
+        X_train, X_test, Y_train, Y_test = train_test_split(gene,
+                                                            hidden_vars[i],
+                                                            test_size=0.2,
+                                                            random_state=42)
+        my_model: RandomForestRegressor = RandomForestRegressor(bootstrap=True, oob_score=False, max_depth=20,
+                                                                random_state=42, n_estimators=100)
+        my_model.fit(X_train, Y_train)
+        explainer = shap.TreeExplainer(my_model)
+        # **explainer = shap.KernelExplainer(my_model.predict, data = X_test.iloc[0:10])
+        shap_values = explainer.shap_values(X_test)
+        # **generate gene model
+        shap_values_mean = np.sum(abs(shap_values), axis=0) / sample_num
+        shap_values_ln = np.log(shap_values_mean)  # *calculate ln^|shap_values_mean|
+        gene_module = np.stack((gene_id, shap_values_ln), axis=0)
+        gene_module = gene_module.T
+        gene_module = gene_module[np.argsort(gene_module[:, 1])]
+        gene_module = gene_module[::-1]
+        gene_model: DataFrame = pd.DataFrame(gene_module)
+        gene_model = gene_model.head(top_num)
+        gene_model = gene_model[(gene_model[[1]] != -np.inf).all(axis=1)]
+        if len(gene_model.index) > (1 / 4) * top_num:
+            gene_model.to_csv(f'{path_to_save_gene_model}({i}).csv', header=False, index=False, sep='\t')
+        # generate bar chart
+        shap.summary_plot(shap_values, X_test, plot_type='bar', plot_size=(15, 10))
+        plt.savefig(f'{path_to_save_bar}({i}).png', dpi=100, format='png')
+        plt.close()
+        # generate scatter chart
+        shap.summary_plot(shap_values, X_test, plot_size=(15, 10))
+        plt.savefig(f'{path_to_save_scatter}({i}).png', dpi=100, format='png')
+        plt.close()
 
-for i in range(column_num):
-    X_train, X_test, Y_train, Y_test = train_test_split(gene,
-                                                        hidden_vars[i],
-                                                        test_size=0.2,
-                                                        random_state=42)
-    my_model = RandomForestRegressor(bootstrap=True, oob_score=False, max_depth=20, random_state=42, n_estimators=100)
-    my_model.fit(X_train, Y_train)
-    explainer = shap.TreeExplainer(my_model)
-    # **explainer = shap.KernelExplainer(my_model.predict, data = X_test.iloc[0:10])
-    shap_values = explainer.shap_values(X_test)
-    # **generate gene module
-    shap_values_mean = np.sum(abs(shap_values), axis=0) / sample_num
-    shap_values_ln = np.log(shap_values_mean)  # *calculate ln^|shap_values_mean|
-    gene_module = np.stack((gene_id, shap_values_ln), axis=0)
-    gene_module = gene_module.T
-    gene_module = gene_module[np.argsort(gene_module[:, 1])]
-    gene_module = gene_module[::-1]
-    gene_module = pd.DataFrame(gene_module)
-    gene_module = gene_module.head(top_num)
-    gene_module = gene_module[(gene_module[[1]] != -np.inf).all(axis=1)]
-    if len(gene_module.index) > (1 / 4) * top_num:
-        gene_module.to_csv(PATH_TO_SAVE_GENE_MODULE + str(i) + '.csv', header=0, index=0, sep='\t')
-    ## generate bar chart
-    shap.summary_plot(shap_values, X_test, plot_type='bar', plot_size=(15, 10))
-    plt.savefig(PATH_TO_SAVE_BAR + str(i) + '.png', dpi=100, format='png')
-    plt.close()
-    ## generate scatter chart
-    shap.summary_plot(shap_values, X_test, plot_size=(15, 10))
-    plt.savefig(PATH_TO_SAVE_SCATTER + str(i) + '.png', dpi=100, format='png')
-    plt.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 6:
+        print('Default setting are used. Either change SHAP_combo.py to change settings or type:\n')
+        print('python SHAP_combo.py path_to_data_gene_name PATH_TO_DATA_GENE_ID PATH_TO_AE_RESULT '
+              'PATH_TO_SAVE_BAR PATH_TO_SAVE_SCATTER PATH_TO_SAVE_GENE_MODULE')
+        print('\tpath_to_data_gene_name - path to cleaned data with gene name after quality control '
+              'e.g. ./gene_name_QC.csv')
+        print('\tPATH_TO_DATA_GENE_ID - path to cleaned data with gene id after quality control e.g ./gene_id_QC.csv')
+        print('\tPATH_TO_AE_RESULT - path to AutoEncoder results, always the last epoch result e.g. ./AE_199.csv')
+        print('\tPATH_TO_SAVE_BAR - path to save SHAP bar chart e.g. ./shap/bar')
+        print('\tPATH_TO_SAVE_SCATTER - path to save SHAP scatter chart e.g. ./shap/scatter')
+        print('\tPATH_TO_SAVE_GENE_MODEL - path to save gene module e.g. ./shap/gene_model')
+        main(Path('./gene_name_QC.csv'), Path('./gene_id_QC.csv'), Path('./AE_199.csv'),
+             Path('./shap/bar'), Path('./shap/scatter'), Path('./shap/gene_module'))
+    else:
+        main(Path(sys.argv[1]), Path(sys.argv[2]), Path(sys.argv[3]),
+             Path(sys.argv[4]), Path(sys.argv[5]), Path(sys.argv[6]))
