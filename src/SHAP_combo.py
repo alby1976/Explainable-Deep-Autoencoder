7696ce7b3e33843a3e9ebc29d5d47d34fb28794c
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-08 20:59:56 -0700

    Fixed Runtime error
    SHAP_combo.py
    Normalize the data before performing
    RandomForestRegressor

--- src/SHAP_combo.py
+++ src/SHAP_combo.py
@@ -30,7 +30,6 @@ def main(path_to_data_gene_name: Path, path_to_data_gene_id: Path, path_to_ae_re
     hidden_vars: DataFrame = pd.read_csv(path_to_ae_result, header=None)
     column_num: int = len(hidden_vars.columns)
     sample_num: int = len(gene.index)
-    gene = minmax_scale(X=gene, feature_range=(0, 1), axis=0, copy=True)
     top_rate: float = 1 / 20  # top rate of gene columns
     top_num: int = int(top_rate * len(gene.columns))
     gene: np.ndarray = minmax_scale(X=gene, feature_range=(0, 1), axis=0, copy=True)
