77a283451e44d4e267b0836c100aec3383d06ff8
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-11 16:26:34 -0700

    Refactored AutoEncoder.py and AutoEncoderModule.py
    Changed code to make things consistent and to set
    batch_size, epoch from the command line
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoder.py
+++ src/AutoEncoder.py
@@ -53,9 +53,9 @@ def run_ae(model_name: str, model: AutoGenoShallow, geno_train_set_loader: DataL
                 coder2 = coder.cpu().detach().numpy()
                 output_coder_list.extend(coder2)
                 # ======precision======
-                input_list.append(train_geno.cpu().detach())
+                input_list.append(train_geno.cpu().detach().)
                 output_list.append(output.cpu().detach())
-                print(f'input:\n{input_list}\noutput:{output_list}')
+                print(f'epoch: {epoch} input:\n{input_list}\nepoch: {epoch} output:{output_list}')
                 # output3 = np.floor(output2 * 3) / 2  # make output3's value to 0, 0.5, 1
                 # diff = geno_data.numpy() - output3  # [0,0.5,1] - [0.0, 0.5, 0.5]
                 # diff_num = np.count_nonzero(diff)
