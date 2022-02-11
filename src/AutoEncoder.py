b758ab67d51b84a46885f936b59bcdf54127aa17
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-10 21:53:06 -0700

    Refactored AutoEncoder.py and AutoEncoderModule.py
    Changed code to make things consistent and to set
    batch_size, epoch from the command line

--- src/AutoEncoder.py
+++ src/AutoEncoder.py
@@ -9,24 +9,96 @@ import torch
 from pathlib import Path
 from pandas import Series, DataFrame
 from sklearn.preprocessing import minmax_scale
+from sklearn.metrics import r2_score
 from torch import nn
+from torch.autograd import Variable
+from torch.optim import Adam
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from AutoEncoderModule import GPDataSet
 from AutoEncoderModule import AutoGenoShallow
-from AutoEncoderModule import run_ae
 from AutoEncoderModule import create_dir
+from AutoEncoderModule import get_normalized_data
 
 
-def get_filtered_data(geno: DataFrame, path_to_save_qc: Path) -> np.ndarray:
+def get_filtered_data(geno: DataFrame, path_to_save_qc: Path) -> DataFrame:
     geno_var: Union[Series, int] = geno.var()
     geno.drop(geno_var[geno_var < 1].index.values, axis=1, inplace=True)
     create_dir(path_to_save_qc.parent)
     geno.to_csv(path_to_save_qc)
-    return np.array(geno)
+    return geno
 
 
-def main(model_name: str, path_to_data: Path, path_to_save_qc: Path, path_to_save_ae: Path, compression_ratio: int):
+def run_ae(model_name: str, model: AutoGenoShallow, geno_train_set_loader: DataLoader, geno_test_set_loader: DataLoader,
+           optimizer: Adam, distance=nn.MSELoss(), num_epochs=200, do_train=True,
+           do_test=True, save_dir: Path = Path('./model')):
+    create_dir(Path(save_dir))
+    for epoch in range(num_epochs):
+        batch_precision_list = []
+        output_coder_list = []
+        average_precision = 0.0
+        sum_loss = 0.0
+        if do_train:
+            current_batch: int = 0
+            model.train()
+            for geno_data in geno_train_set_loader:
+                current_batch += 1
+                train_geno = Variable(geno_data).float().cuda()
+                # =======forward========
+                output, coder = model.forward(train_geno)
+                loss = distance(output, train_geno)
+                sum_loss += loss.item()
+                # ======get coder======
+                coder2 = coder.cpu().detach().numpy()
+                output_coder_list.extend(coder2)
+                # ======precision======
+                # output2 = output.cpu().detach().numpy()
+                # output3 = np.floor(output2 * 3) / 2  # make output3's value to 0, 0.5, 1
+                # diff = geno_data.numpy() - output3  # [0,0.5,1] - [0.0, 0.5, 0.5]
+                # diff_num = np.count_nonzero(diff)
+                # batch_average_precision = 1 - diff_num / (batch_size * input_features)
+                batch_average_precision = r2_score(train_geno, output)
+                batch_precision_list.append(batch_average_precision)
+                # ======backward========
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            # ===========log============
+            coder_np: Union[np.ndarray, int] = np.array(output_coder_list)
+            coder_file = save_dir.joinpath(f"{model_name}-{str(epoch)}.csv")
+            np.savetxt(fname=coder_file, X=coder_np, fmt='%f', delimiter=',')
+            average_precision = sum(
+                batch_precision_list) / current_batch  # precision_list = [ave_pre_batch1, ave_pre_batch2,...]
+        # ===========test==========
+        test_batch_precision_list = []
+        test_average_precision = 0.0
+        test_sum_loss = 0.0
+        if do_test:
+            test_current_batch: int = 0
+            model.eval()
+            for geno_test_data in geno_test_set_loader:
+                test_current_batch += 1
+                test_geno = Variable(geno_test_data).float().cuda()
+                # =======forward========
+                test_output, coder = model.forward(test_geno)
+                loss = distance(test_output, test_geno)
+                test_sum_loss += loss.item()
+                # ======precision======
+                test_output2 = test_output.cpu().detach().numpy()
+                test_output3 = np.floor(test_output2 * 3) / 2  # make output3's value to 0, 0.5, 1
+                # diff = geno_test_data.numpy() - test_output3  # [0,0.5,1] - [0.0, 0.5, 0.5]
+                # diff_num = np.count_nonzero(diff)
+                # batch_average_precision = 1 - diff_num / (batch_size * input_features)  # a single value
+                batch_average_precision = r2_score(test_geno, test_output)
+                test_batch_precision_list.append(batch_average_precision)  # [ave_pre_batch1, ave_pre_batch2,...]
+            test_average_precision = sum(
+                test_batch_precision_list) / test_current_batch
+        print(f"epoch[{epoch + 1:3d}/{num_epochs}, loss: {sum_loss:.4f}, precision: {average_precision:.4f}, "
+              f" test lost: {test_sum_loss:.4f}, test precision: {test_average_precision:.4f}")
+
+
+def main(model_name: str, path_to_data: Path, path_to_save_qc: Path, path_to_save_ae: Path,
+         compression_ratio: int, epoch: int, batch_size: int):
     if not (path_to_save_ae.is_dir()):
         print(f'{path_to_save_ae} is not a directory')
         sys.exit(-1)
@@ -36,11 +108,11 @@ def main(model_name: str, path_to_data: Path, path_to_save_qc: Path, path_to_sav
         sys.exit(-1)
 
     # data quality control
-    geno = get_filtered_data(pd.read_csv(path_to_data, index_col=0), path_to_save_qc)
-    minmax_scale(X=geno, feature_range=(0, 1), axis=0, copy=False)
+    geno: DataFrame = get_filtered_data(pd.read_csv(path_to_data, index_col=0), path_to_save_qc)
     # normalize the data
+    geno: np.ndarray = np.array(get_normalized_data(data=geno))
+    minmax_scale(X=geno, feature_range=(0, 1), axis=0, copy=False)
     # setup of training and testing
-    batch_size = 4096
     geno_train, geno_test = train_test_split(geno, test_size=0.1, random_state=42)
 
     geno_train_set: GPDataSet = GPDataSet(geno)
@@ -58,21 +130,25 @@ def main(model_name: str, path_to_data: Path, path_to_save_qc: Path, path_to_sav
     distance = nn.MSELoss()  # for regression, 0, 0.5, 1
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
     run_ae(model_name=model_name, model=model, geno_train_set_loader=geno_train_set_loader,
-           geno_test_set_loader=geno_test_set_loader, input_features=input_features,
+           geno_test_set_loader=geno_test_set_loader, input_features=input_features, num_epochs=epoch,
            optimizer=optimizer, distance=distance, do_train=True, do_test=True, save_dir=path_to_save_ae)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 5:
+    if len(sys.argv) < 7:
         print('Default setting are used. Either change AutoEncoder.py to change settings or type:\n')
         print('python AutoEncoder.py model_name original_datafile '
-              'quality_control_filename dir_AE_model compression_ratio')
+              'quality_control_filename dir_AE_model compression_ratio epoch batch_size')
         print('\tmodel_name - model name e.g. AE_Geno')
         print('\toriginal_datafile - original datafile e.g. ../data_example.csv')
         print('\tquality_control_filename - filename of original data after quality control e.g. ./data_QC.csv')
         print('\tdir_AE_model - base dir to saved AE models e.g. ./AE')
         print('\tcompression_ratio - compression ratio for smallest layer NB: ideally a number that is power of 2')
+        print('\tepoch - number of iterations e.g. 200')
+        print('\tbatch_size - the size of each batch e.g. 4096')
 
-        main('AE_Geno', Path('../data_example.csv'), Path('./data_QC.csv'), Path('./AE'), 4)
+        main('AE_Geno', Path('../data_example.csv'), Path('./data_QC.csv'), Path('./AE'), 1024, 200, 4096)
     else:
-        main(sys.argv[1], Path(sys.argv[2]), Path(sys.argv[3]), Path(sys.argv[4]), int(sys.argv[5]))
+        main(model_name=sys.argv[1], path_to_data=Path(sys.argv[2]), path_to_save_qc=Path(sys.argv[3]),
+             path_to_save_ae=Path(sys.argv[4]),
+             compression_ratio=int(sys.argv[5]), epoch=int(sys.argv[6]), batch_size=int(sys.argv[7]))
