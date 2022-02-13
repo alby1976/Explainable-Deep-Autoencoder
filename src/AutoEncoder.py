98c220e22d84c5fe1586b35bf42f9828568ddac1
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-13 13:19:54 -0700

    Refactored AutoEncoder.py and AutoEncoderModule.py
    Changed code to make things consistent and to set
    batch_size, epoch from the command line
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoder.py
+++ src/AutoEncoder.py
@@ -2,7 +2,7 @@
 # ** path - is a string to desired path location. **
 import math
 import sys
-from typing import Union
+from typing import Union, Any
 import numpy as np
 import pandas as pd
 import torch
@@ -23,23 +23,37 @@ from AutoEncoderModule import create_dir
 from AutoEncoderModule import get_normalized_data
 
 
-def r2_value(y_true: ndarray, y_pred: ndarray) -> ndarray:
-    # num: int = features*(np.sum(y_true*y_pred) - (y_true.sum()*y_pred.sum()))
-    # den: int = np.sqrt((features * np.sum(np.square(y_true)) - np.square(y_true.sum())) *
-    #                   (features * np.sum(np.square(y_pred)) - np.square(y_pred.sum())))
-    y_ave = y_true.mean(axis=0)
+def calculate_precision (input_data: list, output_data: list) -> float:
+    y_true = np.asarray([x >= 0.5 for x in input_data])
+    y_pred = np.asarray([x >= 0.5 for x in output_data])
+    tp = np.count_nonzero(y_true)
+    fp = np.count_nonzero(np.asarray([x * (x ^ y) for x, y in zip(y_true, y_pred)]))
+    return 1 - tp / (tp + fp)
+
+
+def r2_value(y_true: ndarray, y_pred: ndarray, axis=None) -> tuple:
+    features = y_true.shape[1]
+    num: int = features*(np.sum(y_true*y_pred, axis=axis) - (y_true.sum(axis=axis)*y_pred.sum(axis=axis)))
+    den: int = np.sqrt((features * np.sum(np.square(y_true), axis=axis) - np.square(y_true.sum(axis=axis))) *
+                       (features * np.sum(np.square(y_pred), axis=axis) - np.square(y_pred.sum(axis=axis))))
+    r2: ndarray = np.square(num/den)
+    if (r2.shape != y_true.shape) and (axis == 0):
+        print(f'Method 1 r^2\'s dimension: {r2.shape} input\'s dimension: {y_true.shape}')
+        sys.exit(-1)
+    y_ave = y_true.mean(axis=axis)
     ssr: ndarray = np.square(y_pred - y_ave)
     sst: ndarray = np.square(y_true - y_ave)
-    '''
-    print(f'y_ave {y_ave.shape}: {y_ave}')
-    print(f'y_true {y_true.shape}:\n{y_true}')
-    print(f'y_pred {y_pred.shape}:\n{y_pred}')
-    print(f'ssr {ssr.shape}:\n{ssr}')
-    print(f'sst {ssr.shape}:\n{sst}')
-    print(f'ssr: {ssr.sum(axis=0)}\nsst:\n{sst.sum(axis=0)}\nr^2:\n{ssr.sum(axis=0)/sst.sum(axis=0)}')
-    sys.exit(-1)
-    '''
-    return ssr.sum(axis=0) / sst.sum(axis=0)
+    result = ssr.sum(axis=axis) / sst.sum(axis=axis)
+    if (result.shap != y_true.shape) and (axis == 0):
+        print(f'Method 2 r^2\'s dimension: {result.shape} input\'s dimension: {y_true.shape}')
+        sys.exit(-1)
+
+    return r2, result
+
+
+def adj_r2_value(y_true: ndarray, y_pred: ndarray) -> float:
+    n, k = y_true.shape
+    return 1 - ((1 - r2_value(y_true=y_true, y_pred=y_pred) * (n - 1))/(n - k - 1))
 
 
 def get_filtered_data(geno: DataFrame, path_to_save_qc: Path) -> DataFrame:
@@ -48,104 +62,6 @@ def get_filtered_data(geno: DataFrame, path_to_save_qc: Path) -> DataFrame:
     create_dir(path_to_save_qc.parent)
     geno.to_csv(path_to_save_qc)
     return geno
-
-
-def run_ae(model_name: str, model: AutoGenoShallow, geno_train_set_loader: DataLoader, geno_test_set_loader: DataLoader,
-           features: int, optimizer: Adam, distance=nn.MSELoss(), num_epochs=200, do_train=True,
-           do_test=True, save_dir: Path = Path('./model')):
-    create_dir(Path(save_dir))
-    for epoch in range(num_epochs):
-        batch_precision_list = []
-        output_coder_list = []
-        average_precision = 0.0
-        sum_loss = 0.0
-        if do_train:
-            input_list: ndarray = np.empty((0, features), float)
-            output_list: ndarray = np.empty((0, features), float)
-            current_batch: int = 0
-            model.train()
-            for geno_data in geno_train_set_loader:
-                current_batch += 1
-                train_geno = Variable(geno_data).float().cuda()
-                # =======forward========
-                output, coder = model.forward(train_geno)
-                loss = distance(output, train_geno)
-                sum_loss += loss.item()
-                # ======get coder======
-                coder2 = coder.cpu().detach().numpy()
-                output_coder_list.extend(coder2)
-                # ======precision======
-                # batch_average_precision = r2_score(y_true=geno_data.cpu().detach().numpy(),
-                #                                   y_pred=output.cpu().detach().numpy())
-                input_data = geno_data.numpy()
-                output_data = output.cpu().detach().numpy()
-                y_true = np.asarray([x >= 0.5 for x in input_data])
-                y_pred = np.asarray([x >= 0.5 for x in output_data])
-                tp = np.count_nonzero(y_true)
-                fp = np.count_nonzero(np.asarray([x * (x ^ y) for x, y in zip(y_true, y_pred)]))
-                batch_average_precision = 1 - tp / (tp + fp)
-                #print(f'y_true {y_true.shape}:\n{y_true}')
-                #print(f'y_pred {y_pred.shape}:\n{y_pred}')
-                #print(f'TP: {tp} FP: {fp} precision: {tp/(tp+fp)}')
-
-                # batch_average_precision = np.mean(r2_value(y_true=geno_data.cpu().detach().numpy(),
-                #                                           y_pred=output.cpu().detach().numpy()))
-                # print(f'batch: {current_batch} r2 value: {batch_average_precision}')
-                batch_precision_list.append(batch_average_precision)
-                # input_list = np.append(input_list, geno_data.cpu().detach().numpy(), axis=0)
-                # output_list = np.append(output_list, output.cpu().detach().numpy(), axis=0)
-
-                # ======backward========
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-            # ===========log============
-            coder_np: Union[np.ndarray, int] = np.array(output_coder_list)
-            coder_file = save_dir.joinpath(f"{model_name}-{str(epoch)}.csv")
-            np.savetxt(fname=coder_file, X=coder_np, fmt='%f', delimiter=',')
-            # batch_precision_list = [r2_score_batch1, r2_score_batch2,...]
-            # y_true = np.asarray([x >= 0.5 for x in input_data])
-            # y_pred = np.asarray([x >= 0.5 for x in output_data])
-            # tp = np.count_nonzero(y_true)
-            # fp = np.count_nonzero(np.asarray([x * (x ^ y) for x, y in zip(y_true, y_pred)]))
-            average_precision = np.mean(np.asarray(batch_precision_list))
-        # ===========test==========
-        test_batch_precision_list = []
-        test_average_precision = 0.0
-        test_sum_loss = 0.0
-        if do_test:
-            test_input_list: ndarray = np.empty((0, features), float)
-            test_output_list: ndarray = np.empty((0, features), float)
-            test_current_batch: int = 0
-            model.eval()
-            for geno_test_data in geno_test_set_loader:
-                test_current_batch += 1
-                test_geno = Variable(geno_test_data).float().cuda()
-                # =======forward========
-                test_output, coder = model.forward(test_geno)
-                loss = distance(test_output, test_geno)
-                test_sum_loss += loss.item()
-                # ======precision======
-                # batch_average_precision = r2_score(y_true=geno_test_data.cpu().detach().numpy(),
-                #                                   y_pred=test_output.cpu().detach().numpy())
-                # batch_average_precision = np.mean(r2_value(y_true=geno_test_data.cpu().detach().numpy(),
-                #                                           y_pred=test_output.cpu().detach().numpy()))
-                input_data = geno_test_data.numpy()
-                output_data = test_output.cpu().detach().numpy()
-                y_true = np.asarray([x >= 0.5 for x in input_data])
-                y_pred = np.asarray([x >= 0.5 for x in output_data])
-                tp = np.count_nonzero(y_true)
-                fp = np.count_nonzero(np.asarray([x * (x ^ y) for x, y in zip(y_true, y_pred)]))
-
-                rows, columns = y_true.shape
-                batch_average_precision = 1 - tp / (tp + fp)
-                test_batch_precision_list.append(batch_average_precision)
-                # test_input_list = np.append(test_input_list, geno_test_data.cpu().detach().numpy(), axis=0)
-                # test_output_list = np.append(test_output_list, test_output.cpu().detach().numpy(), axis=0)
-            # test_batch_precision_list = [r2_score_batch1, r2_score_batch2,...]
-            test_average_precision = np.mean(np.asarray(test_batch_precision_list))
-        print(f"epoch[{epoch + 1:3d}/{num_epochs}, loss: {sum_loss:.4f}, precision: {average_precision:.4f}, "
-              f" test lost: {test_sum_loss:.4f}, test precision: {test_average_precision:.4f}")
 
 
 def main(model_name: str, path_to_data: Path, path_to_save_qc: Path, path_to_save_ae: Path,
@@ -167,10 +83,10 @@ def main(model_name: str, path_to_data: Path, path_to_save_qc: Path, path_to_sav
     geno_train, geno_test = train_test_split(geno, test_size=0.1, random_state=42)
 
     geno_train_set: GPDataSet = GPDataSet(geno)
-    geno_train_set_loader = DataLoader(dataset=geno_train_set, batch_size=batch_size, shuffle=False, num_workers=8)
+    geno_train_set_loader: DataLoader[Any] = DataLoader(dataset=geno_train_set, batch_size=batch_size, shuffle=False, num_workers=8)
 
     geno_test_set = GPDataSet(geno_test)
-    geno_test_set_loader = DataLoader(dataset=geno_test_set, batch_size=batch_size, shuffle=False, num_workers=8)
+    geno_test_set_loader: DataLoader[Any] = DataLoader(dataset=geno_test_set, batch_size=batch_size, shuffle=False, num_workers=8)
     input_features = len(geno[0])
     output_features = input_features
     smallest_layer = math.ceil(input_features / compression_ratio)
@@ -181,8 +97,76 @@ def main(model_name: str, path_to_data: Path, path_to_save_qc: Path, path_to_sav
     distance = nn.MSELoss()  # for regression, 0, 0.5, 1
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
     run_ae(model_name=model_name, model=model, geno_train_set_loader=geno_train_set_loader,
-           features=input_features, geno_test_set_loader=geno_test_set_loader, num_epochs=epoch,
+           geno_test_set_loader=geno_test_set_loader, num_epochs=epoch,
            optimizer=optimizer, distance=distance, do_train=True, do_test=True, save_dir=path_to_save_ae)
+
+
+def run_ae(model_name: str, model: AutoGenoShallow, geno_train_set_loader: DataLoader, geno_test_set_loader: DataLoader,
+           optimizer: Adam, distance=nn.MSELoss(), num_epochs=200, do_train=True,
+           do_test=True, save_dir: Path = Path('./model')):
+    create_dir(Path(save_dir))
+    for epoch in range(num_epochs):
+        input_list = []
+        output_list = []
+        precision = 0.0
+        r2 = ()
+        sum_loss = 0.0
+        if do_train:
+            output_coder_list = []
+            model.train()
+            for geno_data in geno_train_set_loader:
+                train_geno = Variable(geno_data).float().cuda()
+                # =======forward========
+                output, coder = model.forward(train_geno)
+                loss = distance(output, train_geno)
+                sum_loss += loss.item()
+                # ======get coder======
+                coder2 = coder.cpu().detach().numpy()
+                output_coder_list.extend(coder2)
+                # ======precision======
+                # batch_average_precision = r2_score(y_true=geno_data.cpu().detach().numpy(),
+                #                                   y_pred=output.cpu().detach().numpy())
+                # ======backward========
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            # ===========log============
+            coder_np: Union[np.ndarray, int] = np.array(output_coder_list)
+            coder_file = save_dir.joinpath(f"{model_name}-{str(epoch)}.csv")
+            np.savetxt(fname=coder_file, X=coder_np, fmt='%f', delimiter=',')
+            # ======precision======
+            precision = calculate_precision(input_data=input_list, output_data=output_list)
+            r2_1: ndarray = r2_value(y_true=input_list, y_pred=output_list)[0]
+            r2_2: ndarray = r2_value(y_true=input_list, y_pred=output_list)[1]
+            r2 = (r2_1.mean(), r2_2.mean(), adj_r2_value(y_true=input_list, y_pred=output_list))
+        # ===========test==========
+        input_list: []
+        output_list: []
+        test_sum_loss = 0.0
+        test_precision = 0.0
+        test_r2: tuple = ()
+        if do_test:
+            model.eval()
+            for geno_test_data in geno_test_set_loader:
+                test_geno = Variable(geno_test_data).float().cuda()
+                # =======forward========
+                test_output, coder = model.forward(test_geno)
+                loss = distance(test_output, test_geno)
+                test_sum_loss += loss.item()
+                # ======precision======
+                # batch_average_precision = r2_score(y_true=geno_test_data.cpu().detach().numpy(),
+                #                                   y_pred=test_output.cpu().detach().numpy())
+                # batch_average_precision = np.mean(r2_value(y_true=geno_test_data.cpu().detach().numpy(),
+                #                                           y_pred=test_output.cpu().detach().numpy()))
+                input_list.append(geno_test_data.numpy())
+                output_list.append(test_output.cpu().detach().numpy())
+            # ======precision======
+            precision = calculate_precision(input_data=input_list, output_data=output_list)
+            r2_1: ndarray = r2_value(y_true=input_list, y_pred=output_list)[0]
+            r2_2: ndarray = r2_value(y_true=input_list, y_pred=output_list)[1]
+            r2 = (r2_1.mean(), r2_2.mean(), adj_r2_value(y_true=input_list, y_pred=output_list))
+        print(f"epoch[{epoch + 1:3d}/{num_epochs}, loss: {sum_loss:.4f}, precision: {precision:.4f}, r2: {r2:.4f}"
+              f" test lost: {test_sum_loss:.4f}, test precision: {test_precision:.4f} test r2: " {test_r2:.4f})
 
 
 if __name__ == '__main__':
