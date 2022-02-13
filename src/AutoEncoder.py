775244cd0e477f5efd48d7c733f542507d18d7d0
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-13 00:08:53 -0700

    Refactored AutoEncoder.py and AutoEncoderModule.py
    Changed code to make things consistent and to set
    batch_size, epoch from the command line
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoder.py
+++ src/AutoEncoder.py
@@ -77,8 +77,8 @@ def run_ae(model_name: str, model: AutoGenoShallow, geno_train_set_loader: DataL
                 # ======precision======
                 # batch_average_precision = r2_score(y_true=geno_data.cpu().detach().numpy(),
                 #                                   y_pred=output.cpu().detach().numpy())
-                y_true = np.asarray([1 if x >= 0.5 else 0 for x in geno_data.numpy()])
-                y_pred = np.asarray([1 if x >= 0.5 else 0 for x in output.cpu().detach().numpy()])
+                y_true = np.asarray([True if x >= 0.5 else False for x in geno_data.numpy()])
+                y_pred = np.asarray([True if x >= 0.5 else False for x in output.cpu().detach().numpy()])
                 tp = np.count_nonzero(y_true)
                 fp = np.count_nonzero(np.asarray([(bool(y) and not(bool(x))) for x, y in zip(y_true, y_pred)]))
 
