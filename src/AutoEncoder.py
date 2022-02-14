5f9b45199312064bc69836d4a1f1d44f9a62857f
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-13 21:34:46 -0700

    Refactored AutoEncoder.py and AutoEncoderModule.py
    Changed code to make things consistent and to set
    batch_size, epoch from the command line
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoder.py
+++ src/AutoEncoder.py
@@ -167,7 +167,7 @@ def run_ae(model_name: str, model: AutoGenoShallow, geno_train_set_loader: DataL
             rows, k = input_list.shape
             n = rows * k
             test_r2 = (tmp1, adj_r2_value(tmp1, n, k))
-        test_sum_loss.append(Series([test_sum_loss]))
+        test_loss_list.append(Series([test_sum_loss]))
         print(f"epoch[{epoch + 1:3d}/{num_epochs}, "
               f"loss: {sum_loss:.4f}, precision: {precision:.4f}, r2: {' '.join(format(r, '.4f') for r in r2)}"
               f" test lost: {test_sum_loss:.4f}, test precision: {test_precision:.4f} "
