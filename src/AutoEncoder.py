5718f75b2eb87c266979ea80c82255eb55fb9d59
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-25 10:34:05 -0700

    Updated AutoEncoderModule.py AutoEncoder.py and CommonTools.py
    fix logical errors
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoder.py
+++ src/AutoEncoder.py
@@ -53,7 +53,7 @@ def main(model_name: str, path_to_data: Path, path_to_save_qc: Path, path_to_sav
                              logger=CSVLogger(save_dir=str(log_dir), name=model_name),
                              deterministic=True,
                              callbacks=[early_stop_r2score],
-                             enable_progress_bar=True
+                             enable_progress_bar=True,
                              auto_scale_batch_size='binsearch')
 
     print('...Finding ideal learning rate....')
@@ -69,12 +69,11 @@ def main(model_name: str, path_to_data: Path, path_to_save_qc: Path, path_to_sav
 
 
 if __name__ == '__main__':
-    '''
     if torch.cuda.is_available():
         device_index = "0"
         os.environ["CUDA_VISIBLE_DEVICES"] = device_index
         os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = 'true'
-'''
+        sys.exit(-1)
 
     if len(sys.argv) < 7:
         print('Default setting are used. Either change AutoEncoder.py to change settings or type:\n')
