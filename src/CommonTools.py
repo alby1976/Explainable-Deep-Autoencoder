a85ab73c646488c18d1ff531cd3e0d2799300976
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-25 08:05:42 -0700

    Refactored AutoEncoderModule.py & CommonTools.py
    Fix logical errors
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/CommonTools.py
+++ src/CommonTools.py
@@ -29,8 +29,8 @@ def get_filtered_data(geno: DataFrame, path_to_save_qc: Path) -> DataFrame:
     geno_var = geno_var[geno_var < 1]
     tmp = geno_var.index.values
     print(f'geno_var:\n{geno_var}\ntmp:\n{tmp}\n')
-    sys.exit(-1)
     geno.drop(geno_var[geno_var < 1].index.values, axis=1, inplace=True)
+    sys.exit(-1)
     create_dir(path_to_save_qc.parent)
     geno.to_csv(path_to_save_qc)
     return geno
