dc2bec52ddde954e8194b65c546a0ad117f738b4
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-02-26 03:37:24 -0700

    Issue: R2 value is negative
    Potential solution:
    Instead of comparing R2 values per node and taking an average
    over all the nodes to present the R2 value for the epoch,
    input and output are flatten before calculating the R2 value
    for the epoch
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoderModule.py
+++ src/AutoEncoderModule.py
@@ -219,8 +219,8 @@ class AutoGenoShallow(pl.LightningModule):
         scheduler: CyclicLR = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=self.min_lr,
                                                                 # mode='exp_range',
                                                                 cycle_momentum=False,
-                                                                step_size_up=50,
-                                                                step_size_down=100,
+                                                                step_size_up=10,
+                                                                step_size_down=20,
                                                                 max_lr=self.learning_rate)
         # step_size = 4 * len(self.train_dataloader())
         # clr = self.cyclical_lr(step_size, min_lr=self.min_lr, max_lr=self.learning_rate)
