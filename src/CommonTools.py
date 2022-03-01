9bf25a79ce6b6e98c77374dc6245e6abad693e4d
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-03-01 08:38:47 -0700

    Issue: R2 value is negative
    Potential solution:
    Instead of comparing R2 values per node and taking an average
    over all the nodes to present the R2 value for the epoch,
    input and output are flatten before calculating the R2 value
    for the epoch
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/CommonTools.py
+++ src/CommonTools.py
@@ -87,11 +87,11 @@ def get_dict_values_2d(key: str, lists: List[Dict[str, Tensor]], dim: int = 0) -
     return torch.cat([item[key] for item in lists], dim=dim)
 
 
-def data_parametric(*samples: Tuple[ndarray, ...]) -> bool:
+def data_parametric(*samples) -> bool:
     # print(f'samples: {type(samples)}\n\n{samples}\n\n')
-    result1, _, _ = same_distribution_test(*samples)
+    result1, _, _ = same_distribution_test(samples)
     result2, _, _ = normality_test(samples[0])
-    result3, _, _ = equality_of_variance_test(*samples)
+    result3, _, _ = equality_of_variance_test(samples)
     return result1 and result2 and result3
 
 
@@ -107,7 +107,7 @@ def same_distribution_test(*samples: Tuple[ndarray, ...]) -> Tuple[bool, float, 
         return True, stat, crit[2]
 
 
-def normality_test(data: Iterable) -> Tuple[bool, float, float]:
+def normality_test(data: ndarray) -> Tuple[bool, float, float]:
     stat: float
     crit: Iterable
 
