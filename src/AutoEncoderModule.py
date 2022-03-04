00e886046b62abe102c1f2574f02b8b57c707a70
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-03-03 22:21:25 -0700

    Refactored AutoEncoderModule.py
    To implement CyclicLR correctly. step_size was calculated
    incorrectly.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoderModule.py
+++ src/AutoEncoderModule.py
@@ -234,7 +234,8 @@ class AutoGenoShallow(LightningModule):
     # configures the optimizers through learning rate
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        it_per_epoch = len(self.train_dataset / self.batch_size
+        it_per_epoch = len(self.train_dataset) / self.batch_size
+        print(f'it_per_epoch: {it_per_epoch}')
         scheduler: CyclicLR = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=self.min_lr,
                                                                 mode='exp_range',
                                                                 cycle_momentum=False,
