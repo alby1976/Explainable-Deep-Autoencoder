b0f62f1539b2f7e3fcf7cc8323ad3e2d7e569a4f
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-03-21 23:08:56 -0600

    Refactored AutoEncoder.py & AutoEncoderModule.py
    modification to code to hand full cancer dataset.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/CommonTools.py
+++ src/CommonTools.py
@@ -169,3 +169,25 @@ def med_var(data, axis=0):
     med = np.median(data, axis=axis)
     tmp = np.median(np.power(data - med, 2), axis=axis)
     return tmp
+
+
+def float_or_none(value: str) -> Optional[float]:
+    """
+    float_or_none.
+
+    Examples:
+        >>> import argparse
+        >>> parser = argparse.ArgumentParser()
+        >>> _ = parser.add_argument('--foo', type=float_or_none)
+        >>> parser.parse_args(['--foo', '4.5'])
+        Namespace(foo=4.5)
+        >>> parser.parse_args(['--foo', 'none'])
+        Namespace(foo=None)
+        >>> parser.parse_args(['--foo', 'null'])
+        Namespace(foo=None)
+        >>> parser.parse_args(['--foo', 'nil'])
+        Namespace(foo=None)
+    """
+    if value.strip().lower() in ("none", "null", "nil"):
+        return None
+    return float(value)
