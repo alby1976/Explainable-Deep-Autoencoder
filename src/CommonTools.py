8f21fd76c50cdc9e98f0d61d9fe85d1e01d4c5de
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-03-23 07:24:43 -0600

    Refactored AutoEncoder.py & AutoEncoderModule.py
    modification to code to hand full cancer dataset.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/CommonTools.py
+++ src/CommonTools.py
@@ -26,18 +26,19 @@ class DataNormalization:
         # the data is log2 transformed and then change to fold change relative to the row's median
         # Those columns whose column modian fold change relative to median is > 0 is keep
         # This module uses MaxABsScaler to scale the data
+        self.column_mask: ndarray = med_var(x_train, axis=0) > 1
         tmp, self.med_fold_change = get_transformed_data(x_train)
-        self.column_mask: ndarray = np.median(tmp, axis=0) > 1
         # print(f'\ntmp: {tmp.shape} fold: {self.med_fold_change.shape} mask: {self.column_mask.shape}')
         # self.column_mask = med_var(x_train, axis=0) > 1
         # tmp, _ = get_transformed_data(x_train[:, self.column_mask], fold=True, median=self.med_fold_change)
         tmp = x_train[:, self.column_mask]
+        tmp, _ = get_transformed_data(tmp, fold=True)
         self.scaler = self.scaler.fit(X=tmp)
         if column_names is not None:
             self.column_names = column_names[self.column_mask]
 
     def transform(self, x: Any):
-        tmp, _ = get_transformed_data(x[:, self.column_mask])
+        tmp, _ = get_transformed_data(x[:, self.column_mask], fold=True)
         if self.column_names is None:
             return self.scaler.transform(X=tmp)
         else:
