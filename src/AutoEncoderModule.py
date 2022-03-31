747554b0babb2653c6335e10593a1543f143478d
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-03-31 13:50:11 -0600

    Refactored
    Modules refactored:
    AutoEncoderModule.py
    CommonTools.py
    
    modification to code to hand full cancer dataset.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoderModule.py
+++ src/AutoEncoderModule.py
@@ -89,7 +89,7 @@ class GPDataModule(pl_bolts.datamodules.SklearnDataModule):
     def split_dataset(self, x, y, val_split: float, test_split: float, random_state: int,
                       fold: bool) -> Tuple[Any, Any, Any, Any, Any, Any]:
         holding_split: float = val_split + test_split
-        if len(np.unique(y)) > 1:
+        if (np.unique(y)).size > 1:
             x_train, x_holding, y_train, y_holding = train_test_split(x, y, test_size=holding_split,
                                                                       random_state=random_state, stratify=y)
         else:
