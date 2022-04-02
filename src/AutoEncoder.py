7ac2146cac42211f679acc3b2d9ba0905567a4d8
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-04-02 08:42:23 -0600

    Refactored AutoEncoder.py & AutoEncoderModule.py
    modification to code to hand full cancer dataset.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoder.py
+++ src/AutoEncoder.py
@@ -99,12 +99,9 @@ def main(args):
 
         # find ideal learning rate and batch_size
         if args.tune:
-            print(f'...Finding ideal batch size....')
+            print(f'...Finding ideal batch size & learning rate....')
             print(f'starting batch size: {model.hparams.batch_size}')
             trainer.tuner.scale_batch_size(model=model, init_val=model.hparams.batch_size, mode='binsearch')
-
-            print('...Finding ideal learning rate....')
-            trainer.tune(model)
             print(f'min lr: {model.lr / 6.0} max lr: {model.lr}')
 
         # train & validate model
