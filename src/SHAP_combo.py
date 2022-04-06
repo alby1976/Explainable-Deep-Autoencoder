dabc4c110197e0dcba6566927f2b47c9233420a6
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-04-06 08:09:47 -0600

    Refactored AutoEncoder.py & AutoEncoderModule.py
    modification to code to hand full cancer dataset.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/SHAP_combo.py
+++ src/SHAP_combo.py
@@ -1,12 +1,14 @@
 # Use Python to plot SHAP figure (include both bar chart and scatter chart) and generate gene module based on SHAP value
 import argparse
+import platform
 from pathlib import Path
-from typing import Any, Optional, Dict
+from typing import Any, Dict, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import shap
+import wandb
 from numpy import ndarray
 from pandas import DataFrame
 from sklearn.ensemble import RandomForestRegressor
@@ -23,92 +25,100 @@ def get_last_model(directory: Path):
     return file_path
 
 
-def main(model_name, gene_name, gene_id, ae_result, col_mask, save_bar, save_scatter, gene_model, fold, test_split,
-         random_state, shuffle):
-    # TODO need to refactor this method to incorporate the changes in x normalization
-    create_dir(Path(save_bar).parent)
-    create_dir(Path(save_scatter).parent)
-    create_dir(Path(gene_model).parent)
-    gene: Optional[DataFrame] = None
-    phen: Optional[DataFrame] = None
-    geno_id: DataFrame
+def main(model_name, gene_name, gene_id, ae_result, col_mask, save_bar, save_scatter, gene_model, num_workers, fold,
+         test_split, random_state, shuffle):
+    with wandb.init(name=model_name, project="XAE4Exp"):
+        # wandb configuration
+        wandb.config.update = {"architecture": platform.platform(),
+                               "Note": f"stratify splitting if there more than 2 phenotype and each category has more "
+                                       f"than 1 element; random_state={random_state}"}
+        create_dir(Path(save_bar).parent)
+        create_dir(Path(save_scatter).parent)
+        create_dir(Path(gene_model).parent)
+        geno_id: DataFrame
 
-    mask: pd.DataFrame = get_data(col_mask)
-    geno_id, phen = get_phen(get_data(gene_id))
+        mask: pd.DataFrame = get_data(col_mask)
+        geno_id, phen = get_phen(get_data(gene_id))
 
-    if gene_name is None:
-        gene = convert_gene_id_to_name(geno_id, mask.columns.to_numpy())
-    else:
-        gene, phen = get_phen(get_data(gene_name))
-
-    hidden_vars: DataFrame = get_data(ae_result, index_col=None, header=None)
-    column_num: int = len(hidden_vars.columns)
-    sample_num: int = len(gene.index)
-    top_rate: float = 1 / 20  # top rate of gene columns
-    top_num: int = int(top_rate * len(gene.columns))
-    ids: ndarray = geno_id.columns.to_numpy()[mask.values.flatten()]
-    unique, unique_count = np.unique(phen, return_counts=True)
-    print(f"\ndf mask:\n{mask}\nnp mask:\n{mask.to_numpy()}\n")
-    dm = DataNormalization(column_mask=mask.values.flatten())
-
-    for i in range(column_num):
-        x_train: Any
-        x_test: Any
-        y_train: Any
-        y_test: Any
-        phen_train: Any
-        phen_test: Any
-        if phen is not None and unique.size > 1 and np.min(unique_count) > 1:
-            x_train, x_test, y_train, y_test, phen_train, phen_test = train_test_split(gene.to_numpy(),
-                                                                                       hidden_vars[i],
-                                                                                       phen.to_numpy(),
-                                                                                       test_size=test_split,
-                                                                                       shuffle=shuffle,
-                                                                                       stratify=phen,
-                                                                                       random_state=random_state)
+        if gene_name is None:
+            gene = convert_gene_id_to_name(geno_id, mask.columns.to_numpy())
         else:
-            x_train, x_test, y_train, y_test, = train_test_split(gene,
-                                                                 hidden_vars[i],
-                                                                 test_size=test_split,
-                                                                 shuffle=shuffle,
-                                                                 stratify=phen,
-                                                                 random_state=random_state)
+            gene, phen = get_phen(get_data(gene_name))
 
-        my_model: RandomForestRegressor = RandomForestRegressor(bootstrap=True, oob_score=False, max_depth=20,
-                                                                random_state=random_state, n_estimators=100)
-        print(f"\nx_train: {x_train.shape} y_train: {y_train.shape} phen_train: {phen_train.shape}")
-        dm.fit(x_train, fold)
-        my_model.fit(dm.transform(x_train, fold), y_train)
-        explainer = shap.TreeExplainer(my_model)
-        # **explainer = shap.KernelExplainer(my_model.predict, x = x_test.iloc[0:10])
-        shap_values = explainer.shap_values(dm.transform(x_test, fold))
-        # **generate gene model
-        shap_values_mean = np.sum(abs(shap_values), axis=0) / sample_num
-        shap_values_ln = np.log(shap_values_mean)  # *calculate ln^|shap_values_mean|
-        gene_module = np.stack((ids, shap_values_ln), axis=0)
-        gene_module = gene_module.T
-        gene_module = gene_module[np.argsort(gene_module[:, 1])]
-        gene_module = gene_module[::-1]  # [starting index: stopping index: stepcount]
-        gene_model: DataFrame = pd.DataFrame(gene_module)
-        gene_model = gene_model.head(top_num)
-        masking = gene_model[[1]] != -np.inf
-        print(f'\ngene_model:\n{gene_model}\n')
-        print(f'\nmask:\n{masking}\n')
-        sys.exit(-1)
-        gene_model = gene_model[masking.all(axis=1)]
-        if len(gene_model.index) > (1 / 4) * top_num:
-            print(f'{gene_model}({i}).csv')
-            gene_model.to_csv(f'{gene_model}({i}).csv', header=True, index=False, sep='\t')
-        # generate bar chart
-        shap.summary_plot(shap_values, x_test, plot_type='bar', plot_size=(15, 10))
-        print(f'{save_bar}({i}).png')
-        plt.savefig(f'{save_bar}({i}).png', dpi=100, format='png')
-        plt.close()
-        # generate scatter chart
-        shap.summary_plot(shap_values, x_test, plot_size=(15, 10))
-        print(f'{save_scatter}({i}).png')
-        plt.savefig(f'{save_scatter}({i}).png', dpi=100, format='png')
-        plt.close()
+        hidden_vars: DataFrame = get_data(ae_result, index_col=None, header=None)
+        column_num: int = len(hidden_vars.columns)
+        sample_num: int = len(gene.index)
+        top_rate: float = 1 / 20  # top rate of gene columns
+        top_num: int = int(top_rate * len(gene.columns))
+        ids: ndarray = geno_id.columns.to_numpy()[mask.values.flatten()]
+        unique, unique_count = np.unique(phen, return_counts=True)
+        print(f"\ndf mask:\n{mask}\nnp mask:\n{mask.to_numpy()}\n")
+        dm = DataNormalization(column_mask=mask.values.flatten())
+
+        for i in range(column_num):
+            x_train: Any
+            x_test: Any
+            y_train: Any
+            y_test: Any
+            phen_train: Any = None
+            phen_test: Any
+            if phen is not None and unique.size > 1 and np.min(unique_count) > 1:
+                x_train, x_test, y_train, y_test, phen_train, phen_test = train_test_split(gene.to_numpy(),
+                                                                                           hidden_vars[i],
+                                                                                           phen.to_numpy(),
+                                                                                           test_size=test_split,
+                                                                                           shuffle=shuffle,
+                                                                                           stratify=phen,
+                                                                                           random_state=random_state)
+            else:
+                x_train, x_test, y_train, y_test, = train_test_split(gene,
+                                                                     hidden_vars[i],
+                                                                     test_size=test_split,
+                                                                     shuffle=shuffle,
+                                                                     stratify=phen,
+                                                                     random_state=random_state)
+
+            my_model: RandomForestRegressor = RandomForestRegressor(bootstrap=True, oob_score=False, max_depth=20,
+                                                                    random_state=random_state, n_estimators=100,
+                                                                    n_jobs=num_workers)
+            print(f"\nx_train: {x_train.shape} y_train: {y_train.shape} phen_train: {phen_train.shape}")
+            dm.fit(x_train, fold)
+            my_model.fit(dm.transform(x_train, fold), y_train)
+            explainer = shap.TreeExplainer(my_model)
+            # **explainer = shap.KernelExplainer(my_model.predict, x = x_test.iloc[0:10])
+            shap_values = explainer.shap_values(dm.transform(x_test, fold))
+            # **generate gene model
+            shap_values_mean = np.sum(abs(shap_values), axis=0) / sample_num
+            shap_values_ln = np.log(shap_values_mean)  # *calculate ln^|shap_values_mean|
+            gene_module: Union[ndarray, DataFrame] = np.stack((ids, shap_values_ln), axis=0)
+            gene_module = gene_module.T
+            gene_module = gene_module[np.argsort(gene_module[:, 1])]
+            gene_module = gene_module[::-1]  # [starting index: stopping index: stepcount]
+            gene_module = pd.DataFrame(gene_module)
+            gene_module = gene_module.head(top_num)
+            masking: Union[ndarray, bool] = gene_module[[1]] != -np.inf
+            print(f'\ngene_model:\n{gene_module}\n')
+            print(f'\nmask:\n{masking}\n')
+            gene_module = gene_module[masking.all(axis=1)]
+            if len(gene_module.index) > (1 / 4) * top_num:
+                print(f'{gene_model}({i}).csv')
+                gene_module.to_csv(f'{gene_model}({i}).csv', header=True, index=False, sep='\t')
+                tbl = wandb.Table(dataframe=gene_module, dtype=float)
+                wandb.log({f"{model_name}({i})": tbl})
+            # generate bar chart
+            shap.summary_plot(shap_values, x_test, plot_type='bar', plot_size=(15, 10))
+            print(f'{save_bar}({i}).png')
+            plt.savefig(f'{save_bar}({i}).png', dpi=100, format='png')
+            plt.close()
+            tmp = f"{model_name}-bar-({i})"
+            wandb.log({tmp: wandb.Image(f"{save_bar}({i}).png")})
+            # generate scatter chart
+            shap.summary_plot(shap_values, x_test, plot_size=(15, 10))
+            print(f'{save_scatter}({i}).png')
+            plt.savefig(f'{save_scatter}({i}).png', dpi=100, format='png')
+            plt.close()
+            tmp = f"{model_name}-scatter-({i})"
+            wandb.log({tmp: wandb.Image(f"{save_scatter}({i}).png")})
 
 
 if __name__ == '__main__':
@@ -132,6 +142,9 @@ if __name__ == '__main__':
     parser.add_argument("-m", "--gene_model", type=Path,
                         default=Path(__file__).absolute().parent.parent.joinpath("shap/bar"),
                         help='path to save gene module e.g. ./shap/gene_model')
+    parser.add_argument("-w", "--num_workers", type=int,
+                        help='number of processors used to run in parallel. -1 mean using all processor available '
+                             'default is None')
     parser.add_argument("--fold", type=bool, default=False,
                         help='selecting this flag causes the x to be transformed to change fold relative to '
                              'row median. default is False')
