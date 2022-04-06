2b46010e5586479bf9de749930e7141311afbe78
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-04-05 20:04:11 -0600

    Refactored AutoEncoder.py & AutoEncoderModule.py
    modification to code to hand full cancer dataset.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/SHAP_combo.py
+++ src/SHAP_combo.py
@@ -49,7 +49,7 @@ def main(model_name, gene_name, gene_id, ae_result, col_mask, save_bar, save_sca
     ids: ndarray = geno_id.columns.to_numpy()
     unique, unique_count = np.unique(phen, return_counts=True)
     print(f"\ndf mask:\n{mask}\nnp mask:\n{mask.to_numpy()}\n")
-    dm = DataNormalization(column_mask=mask.to_numpy())
+    dm = DataNormalization(column_mask=mask.values)
 
     for i in range(column_num):
         x_train: Any
