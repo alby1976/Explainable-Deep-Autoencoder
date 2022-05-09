07ae620ed803ad5b77f6a35531ea7b21c657ac10
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-05-08 23:52:19 -0600

    Modified AutoEncoder AutoEncoderModule.py ShapDeepExplainerModule
    modification to code for ShapDeepExplainer.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/AutoEncoder.py
+++ src/AutoEncoder.py
@@ -119,7 +119,7 @@ def main(args):
             tbl = wandb.Table(dataframe=pd.DataFrame(hidden_layer), dtype=float)
             wandb.log({"AE_out": tbl})
 
-    create_shap_values(model, args.model_name + "_Shap", args.save_dir.joinpath(args.gene_model),
+    create_shap_values(model, args.name + "_Shap", args.save_dir.joinpath(args.gene_model),
                        args.save_dir.joinpath(args.save_bar), args.save_dir.joinpath(args.save_scatter), args.top_rate)
 
 
