7712993d1239caef750fa9b200ca4112ff7b084a
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-05-09 20:48:44 -0600

    Modified AutoEncoder AutoEncoderModule.py ShapDeepExplainerModule
    modification to code for ShapDeepExplainer.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/ShapDeepExplainerModule.py
+++ src/ShapDeepExplainerModule.py
@@ -12,7 +12,7 @@ import wandb
 from matplotlib import pyplot as plt
 from numpy import ndarray
 from pandas import DataFrame
-from torch import nn
+from torch import nn, Tensor
 
 from AutoEncoderModule import AutoGenoShallow
 
@@ -69,7 +69,10 @@ def create_shap_values(model: AutoGenoShallow, model_name: str, gene_model: Path
     genes, _ = batch
     x_train = genes[:100]
 
-    x_test = torch.cat([batch[0] for batch in model.val_dataloader()])
+    x_test: Tensor = torch.cat([batch[0] for batch in model.val_dataloader()])
+    print(f"batch type: {type(batch)} batch size: {len(batch)}\n{batch}\n\n")
+    print(f"x_train type: {type(x_train)}\n{x_train}\n\n")
+    print(f"x_test type: {type(x_test)} x_test size: {len(x_test)\n")
 
     gene_names: ndarray = model.dataset.gene_names[model.dataset.dm.column_mask]
     top_num: int = int(top_rate * len(gene_names))  # top_rate is the percentage of features to be calculated
