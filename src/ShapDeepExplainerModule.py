d6e5f769e55c65b4d9298ade4354af9c9b75db42
Albert Leung <1319401+alby1976@users.noreply.github.com>
2022-05-12 12:00:11 -0600

    Modified AutoEncoder AutoEncoderModule.py ShapDeepExplainerModule
    modification to code for ShapDeepExplainer.
    
    Signed-off-by: Albert Leung <1319401+alby1976@users.noreply.github.com>

--- src/ShapDeepExplainerModule.py
+++ src/ShapDeepExplainerModule.py
@@ -1,6 +1,7 @@
 # python system library
 # 3rd party modules
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from itertools import repeat
 
 from matplotlib import pyplot as plt
 from torch import nn
@@ -52,7 +53,7 @@ def process_shap_values(save_bar: Path, save_scatter: Path, gene_model: Path, mo
 
 
 def create_shap_values(model: AutoGenoShallow, model_name: str, gene_model: Path, save_bar: Path, save_scatter: Path,
-                       top_rate: float = 0.05):
+                       top_rate: float = 0.05, num_workers: int = 8):
     shap_values: Union[
         ndarray, List[ndarray], Tuple[List[Union[ndarray, List[ndarray]]], Any], List[Union[ndarray, List[ndarray]]]]
 
@@ -95,17 +96,18 @@ def create_shap_values(model: AutoGenoShallow, model_name: str, gene_model: Path
     explainer = shap.DeepExplainer(model, x_train)
     shap_values = explainer.shap_values(x_test, top_num, "max")  # shap_values contains values for all nodes
     x_test = x_test.detach().cpu().numpy()
-    with ThreadPoolExecutor(max_workers=8) as pool:
-        params = ((save_bar, save_scatter, gene_model, model_name, x_test, shap_values, gene_names,
-                   sample_size, top_num, node) for node in shap_values)
-        pool.map(lambda x: process_shap_values(*x), params)
+    with ProcessPoolExecutor(max_workers=num_workers) as pool:
+        params = (save_bar, save_scatter, gene_model, model_name, x_test, shap_values, gene_names,
+                  sample_size, top_num)
+        print(f"calling with : {params}")
+        pool.map(process_shap_values, repeat(params), shap_values)
 
 
 def main(ckpt: Path, model_name: str, gene_model: Path, save_bar: Path, save_scatter: Path,
-         top_rate: float = 0.05):
+         top_rate: float = 0.05, num_workers: int = 8):
     # model = AutoGenoShallow()
     model = AutoGenoShallow.load_from_checkpoint(str(ckpt))
-    create_shap_values(model, model_name, gene_model, save_bar, save_scatter, top_rate)
+    create_shap_values(model, model_name, gene_model, save_bar, save_scatter, top_rate, num_worker=num_workers)
 
 
 if __name__ == '__main__':
@@ -135,4 +137,5 @@ if __name__ == '__main__':
     print(f"args:\n{args}")
 
     main(args.save_dir.joinpath(args.ckpt), args.name, args.save_dir.joinpath(args.gene_model),
-         args.save_dir.joinpath(args.save_bar), args.save_dir.joinpath(args.save_scatter), args.top_rate)
+         args.save_dir.joinpath(args.save_bar), args.save_dir.joinpath(args.save_scatter),
+         args.top_rate, args.num_workers)
